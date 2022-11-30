d4da63390e241fe0eb17204a5a9871a697f44a46
MiguelH <miguelhh38@educastur.es>
2022-11-30 19:48:24 +0100

    Controllers Menu principal y registro

--- src/main/java/com/example/PeregrinosFX/controller/MenuPrincipalController.java
+++ src/main/java/com/example/PeregrinosFX/controller/MenuPrincipalController.java
@@ -1,6 +1,22 @@
 package com.example.PeregrinosFX.controller;
 
 
-public class MenuPrincipalController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuPrincipalController implements Initializable {
+    @FXML
+    private Button iniciarsesionBTN;
+
+    @FXML
+    private Button registrarseBTN;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
--- src/main/java/com/example/PeregrinosFX/controller/RegistroController.java
+++ src/main/java/com/example/PeregrinosFX/controller/RegistroController.java
@@ -1,4 +1,61 @@
 package com.example.PeregrinosFX.controller;
 
-public class RegistroController {
+import javafx.fxml.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+public class RegistroController implements Initializable {
+
+    @FXML
+    private Label nombreLBL;
+
+    @FXML
+    private Label usuarioLBL;
+
+    @FXML
+    private Label contrasenaLBL;
+
+    @FXML
+    private Label confirmcontraLBL;
+
+    @FXML
+    private Label paradaLBL;
+
+    @FXML
+    private Label nacionalidadLBL;
+
+    @FXML
+    private TextField nombreTF;
+
+    @FXML
+    private TextField usuarioTF;
+
+    @FXML
+    private TextField contrasenaTF;
+
+    @FXML
+    private TextField confirmcontraTF;
+
+    @FXML
+    private ComboBox paradaCB;
+
+    @FXML
+    private ComboBox nacionalidadCB;
+
+    @FXML
+    private Button aceptarBTN;
+
+    @FXML
+    private Button cancelarBTN;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
