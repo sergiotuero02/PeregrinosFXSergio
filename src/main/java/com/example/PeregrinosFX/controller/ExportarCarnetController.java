c4f3ff81ecbe1eb81648f777add085e7514459ea
sergi <sergiotg59@educastur.es>
2022-12-01 17:32:28 +0100

    ajuste de controllers

--- src/main/java/com/example/PeregrinosFX/controller/ExportarCarnetController.java
+++ src/main/java/com/example/PeregrinosFX/controller/ExportarCarnetController.java
@@ -1,14 +1,23 @@
 package com.example.PeregrinosFX.controller;
 
 import com.example.PeregrinosFX.config.StageManager;
+import javafx.fxml.Initializable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @Controller
-public class ExportarCarnetController {
+public class ExportarCarnetController implements Initializable{
 
     @Lazy
     @Autowired
     private StageManager stageManager;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
--- src/main/java/com/example/PeregrinosFX/controller/UsuarioCreadoController.java
+++ src/main/java/com/example/PeregrinosFX/controller/UsuarioCreadoController.java
@@ -1,14 +1,23 @@
 package com.example.PeregrinosFX.controller;
 
 import com.example.PeregrinosFX.config.StageManager;
+import javafx.fxml.Initializable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @Controller
-public class UsuarioCreadoController {
+public class UsuarioCreadoController implements Initializable {
 
     @Lazy
     @Autowired
     private StageManager stageManager;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
