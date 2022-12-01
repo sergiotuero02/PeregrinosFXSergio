4d5114f02dc22e1900589003730045f02225d82d
sergi <sergiotg59@educastur.es>
2022-12-01 16:34:49 +0100

    cambio de pantalla

--- src/main/java/com/example/PeregrinosFX/controller/MenuPrincipalController.java
+++ src/main/java/com/example/PeregrinosFX/controller/MenuPrincipalController.java
@@ -1,21 +1,32 @@
 package com.example.PeregrinosFX.controller;
 
 
+import com.example.PeregrinosFX.config.StageManager;
+import com.example.PeregrinosFX.view.FxmlView;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import org.springframework.stereotype.Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 @Controller
 public class MenuPrincipalController implements Initializable {
+    private StageManager stageManager;
     @FXML
     private Button iniciarsesionBTN;
 
+
     @FXML
     private Button registrarseBTN;
+
+    @FXML
+    private void abrirRegistro(ActionEvent event) throws IOException{
+        stageManager.switchScene(FxmlView.REGISTRO);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
