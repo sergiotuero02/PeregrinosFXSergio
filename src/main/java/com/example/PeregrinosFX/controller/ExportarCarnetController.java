34adb099e5e18c7974b2bb45751ec1ed06343e1c
sergiotuero02 <92050177+sergiotuero02@users.noreply.github.com>
2022-12-14 16:07:55 +0100

    Exportar carnet

--- src/main/java/com/example/PeregrinosFX/controller/ExportarCarnetController.java
+++ src/main/java/com/example/PeregrinosFX/controller/ExportarCarnetController.java
@@ -1,17 +1,22 @@
 package com.example.PeregrinosFX.controller;
 
+import com.example.PeregrinosFX.bean.Parada;
+import com.example.PeregrinosFX.bean.Peregrino;
 import com.example.PeregrinosFX.config.StageManager;
+import com.example.PeregrinosFX.service.PeregrinoService;
 import com.example.PeregrinosFX.view.FxmlView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import static com.example.PeregrinosFX.controller.LoginController.rol;
@@ -23,8 +28,15 @@ public class ExportarCarnetController implements Initializable{
     @Autowired
     private StageManager stageManager;
 
+    @Lazy
+    @Autowired
+    private PeregrinoService peregrinoService;
+
     @FXML
     private Button expcarnetBTN;
+
+    @FXML
+    private ComboBox peregrinosCB;
 
     @FXML
     private Button cancelarBTN;
@@ -52,6 +64,10 @@ public class ExportarCarnetController implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        ArrayList<Peregrino> peregrinos = new ArrayList<>();
+        peregrinos = (ArrayList<Peregrino>) peregrinoService.findAll();
+        for (Peregrino p1 : peregrinos) {
+            peregrinosCB.getItems().add(p1);
+        }
     }
 }
